9203c16315278e39ef84d3db7b4e08b6a185f068
Ahmad Fajar Prasetiyo <me@ahmadfajar.com>
2024-02-14 15:43:52 +0700

    Stage 4/5: Zebra crossing

--- QRCode Service/task/src/qrcodeapi/Controller.java
+++ QRCode Service/task/src/qrcodeapi/Controller.java
@@ -1,5 +1,10 @@
 package qrcodeapi;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,20 +23,31 @@ public class Controller {
     }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<BufferedImage> qrcode(@RequestParam(required = false, name = "size") Integer size, @RequestParam(required = false, name = "type") String type) {
-        List<String> validType = List.of("png", "jpeg", "gif");
+    public ResponseEntity<BufferedImage> qrcode(@RequestParam(required = false, name = "size") Integer size, @RequestParam(required = false, name = "type") String type, @RequestParam(required = false, name = "contents") String contents) {
+
+
+        if (contents == null || contents.trim().isEmpty()) {
+            throw new RuntimeException("Contents cannot be null or blank");
+        }
+
         if (size == null || size < 150 || size > 350) {
             throw new RuntimeException("Image size must be between 150 and 350 pixels");
         }
 
+        BufferedImage bufferedImage;
+
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(contents, BarcodeFormat.QR_CODE, size, size);
+            bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+        } catch (WriterException e) {
+            throw new RuntimeException("Contents cannot be null or blank");
+        }
+
+        List<String> validType = List.of("png", "jpeg", "gif");
         if (type == null || !validType.contains(type)) {
             throw new RuntimeException("Only png, jpeg and gif image types are supported");
         }
-        BufferedImage bufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = bufferedImage.createGraphics();
-
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, size, size);
 
         MediaType mediaType = MediaType.IMAGE_PNG;
         if (type.equals("jpeg")) {
